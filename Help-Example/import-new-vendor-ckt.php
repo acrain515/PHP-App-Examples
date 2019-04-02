aea5f49ff3778a66209f37973e34aa767573c8f9
acrain515 <49204345+acrain515@users.noreply.github.com>
2019-04-02 13:57:00 -0400

    Add files via upload

---
+++ Help-Example/import-new-vendor-ckt.php
@@ -0,0 +1,51 @@
+<style>
+body { font-family:Arial;font-size:12px;padding:40px; }
+h2 { font-size:28px; }
+h2 { font-size:20px; }
+</style>
+
+<h2>Import New Vendor Ckt Help</h2>
+
+The Import New Vendor Ckt tool allows you to import multiple new LEC Circuit IDs into Utopia at one time.  
+</br></br></br>
+
+<b>Step 1:  Create an Excel Spreadsheet with 2 Columns </b></br>
+Column 1 should be the existing Clean Vendor Circuit ID already in Utopia </br>
+Column 2 should be the New Vendor Ckt </br>
+
+</br></br>
+
+<b>Step 2: Populate the Fields On Your Spreadsheet </b></br>
+Your spreadsheet should look similar to this screenshot </br>
+<img src="images/new-vendor-ckt-excel-1.png"> </br>
+
+</br></br>
+
+<b>Step 3: "Save As" Standard CSV Format </b></br>
+<img src="images/new-vendor-ckt-excel-2.png"></br>
+
+</br></br>
+
+<b>Step 4: Click Choose File in Utopia and Select the CSV Just Saved </b></br>
+
+</br></br>
+
+<b>Step 5: Click Import in Utopia</b></br>
+You should see a "Successful" message for each record that was updated, similar to the screenshot below.</br>
+You can now close this browser window and refresh the tracking page.</br>
+<img src="images/new-vendor-ckt-5.png"></br>
+
+</br></br>
+<h3>Troubleshooting:</h3>
+<b>Issue:</b></br> Error message "incorrect number of columns" received.  </br></br>
+<b>Solution:</b></br>  Make sure you only have two columns in your spreadsheet. 
+If you did not start out with a blank template, you may have more than two columns in your CSV file, </br>
+even if there is no data in those columns.</br>  
+Edit your csv by highlighting the entire length of columns (after the second column) in your CSV file,</br> 
+resave the file and try again. </br> 
+
+</br>
+</br>
+</br>
+</br>
+
